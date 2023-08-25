f33d41ab96d103247fc57d252586638e9201ac36
Diiego Camiino <dieguitokmino@gmail.com>
2023-08-25 00:41:50 -0500

    Se termino eliminar

--- error403.php
+++ error403.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles403.css">
+    <link rel="stylesheet" href="css/403.css">
     <link rel="icon" href="svg/icon.svg">
     <title>Eror 403</title>
 </head>
--- error404.php
+++ error404.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles404.css">
+    <link rel="stylesheet" href="css/404.css">
     <link rel="icon" href="svg/icon.svg">
     <title>Eror 404</title>
 </head>
